211254c274d84bf9e493c4925a03966e63bc871c
Servicios Digitales <raf.albornoz@duocuc.cl>
2025-10-22 10:01:19 -0300

    hola

--- app/src/main/java/com/example/puntobaselavenganza/ApiMindicador/CargarListaUtm.kt
+++ app/src/main/java/com/example/puntobaselavenganza/ApiMindicador/CargarListaUtm.kt
@@ -3,26 +3,36 @@ package com.example.puntobaselavenganza.ApiMindicador
 import android.widget.Toast
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
+import com.example.puntobaselavenganza.MainActivity
 import kotlinx.coroutines.launch
 
+
 object CargarListaUtm {
-    fun cargaUtmMindicador(owner: LifecycleOwner){
+    fun cargaUtmMindicador(owner: LifecycleOwner, mainActivity: MainActivity){
         owner.lifecycleScope.launch {
             val respuesta = MindiRepo.fetchApiMin()
 
             respuesta.onSuccess { utmList ->
                 if (utmList.isEmpty()) {
                     Toast.makeText(
-                        context,
+                        mainActivity,
                         "Sin datos desde el servicio",
                         Toast.LENGTH_SHORT
                     ).show()
                 }
 
-                // Hacer variable
-                // Hacer lista y ingresar los valores
+                val listaValoresInt: MutableList<Int> = utmList.map { it.valor }.toMutableList()
+
+                var primerValor: Int = listaValoresInt[0]
+
+                traerValor(primerValor)
 
             }
+
         }
+    }
+
+    fun traerValor(primerUtm : Int): Int{
+        // Calcular
     }
 }
